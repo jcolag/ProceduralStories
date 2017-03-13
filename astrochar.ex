2653daea7212e185e07c26eee2e6d7b07efdd364
John Colagioia <john@colagioia.net>
2017-03-13 07:30:43 -0400

    Add structure for planets

--- astrochar.ex
+++ astrochar.ex
@@ -2,4 +2,8 @@ defmodule Trait do
   defstruct id: 0, name: "Trait", negname: "Antitrait"
 end
 
+defmodule Planet do
+  defstruct id: 0, name: "Planet", aradius: 1000000, bradius: 1000000, traitid: 0
+end
+
 
