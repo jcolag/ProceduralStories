368e5e14d26e11b89da56d9e4ceeb2ecb6d7d616
John Colagioia <john@colagioia.net>
2017-03-16 07:13:25 -0400

    Add building blocks of signs

--- astrochar.ex
+++ astrochar.ex
@@ -6,6 +6,14 @@ defmodule Planet do
   defstruct id: 0, name: "Planet", aradius: 1000000, bradius: 1000000, traitid: 0, positive: true
 end
 
+defmodule Element do
+  defstruct id: 0, name: "Element"
+end
+
+defmodule Quality do
+  defstruct id: 0, name: "Quality"
+end
+
 defmodule Constellation do
   defstruct id: 0, name: "Constellation", angle: 0, traitid: 0, positive: true
 end
