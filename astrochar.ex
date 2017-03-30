cc68eca6e7dceb826b895f29896d19ec2fc73883
John Colagioia <john@colagioia.net>
2017-03-30 07:12:57 -0400

    Add stubs for the other planets

--- astrochar.ex
+++ astrochar.ex
@@ -35,6 +35,16 @@ end
 defmodule Orrery do
   defp init() do
     p1 = %Planet{id: 0, name: "Sun", mass: 1988550000, centerid: -1, focusangle: 0, focusdist: 0, aradius: 1, bradius: 1, traitid: 0, positive: true, currangle: 0}
+    p2 = %Planet{id: 1, name: "Mercury", mass: 1988550000, centerid: 0, focusangle: 0, focusdist: 0, aradius: 1, bradius: 1, traitid: 0, positive: true, currangle: 0}
+    p3 = %Planet{id: 2, name: "Venus", mass: 1988550000, centerid: 0, focusangle: 0, focusdist: 0, aradius: 1, bradius: 1, traitid: 0, positive: true, currangle: 0}
+    p4 = %Planet{id: 3, name: "Earth", mass: 1988550000, centerid: 0, focusangle: 0, focusdist: 0, aradius: 1, bradius: 1, traitid: 0, positive: true, currangle: 0}
+    p5 = %Planet{id: 4, name: "Moon", mass: 1988550000, centerid: 3, focusangle: 0, focusdist: 0, aradius: 1, bradius: 1, traitid: 0, positive: true, currangle: 0}
+    p6 = %Planet{id: 5, name: "Mars", mass: 1988550000, centerid: 0, focusangle: 0, focusdist: 0, aradius: 1, bradius: 1, traitid: 0, positive: true, currangle: 0}
+    p7 = %Planet{id: 6, name: "Jupiter", mass: 1988550000, centerid: 0, focusangle: 0, focusdist: 0, aradius: 1, bradius: 1, traitid: 0, positive: true, currangle: 0}
+    p8 = %Planet{id: 7, name: "Saturn", mass: 1988550000, centerid: 0, focusangle: 0, focusdist: 0, aradius: 1, bradius: 1, traitid: 0, positive: true, currangle: 0}
+    p9 = %Planet{id: 8, name: "Uranus", mass: 1988550000, centerid: 0, focusangle: 0, focusdist: 0, aradius: 1, bradius: 1, traitid: 0, positive: true, currangle: 0}
+    p10 = %Planet{id: 9, name: "Neptune", mass: 1988550000, centerid: 0, focusangle: 0, focusdist: 0, aradius: 1, bradius: 1, traitid: 0, positive: true, currangle: 0}
+    p11 = %Planet{id: 10, name: "Pluto", mass: 1988550000, centerid: 0, focusangle: 0, focusdist: 0, aradius: 1, bradius: 1, traitid: 0, positive: true, currangle: 0}
   end
 end
 
