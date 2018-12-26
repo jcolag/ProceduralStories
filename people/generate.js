16e3ce71268f6d94aa18c0aa73e712b66b20f865
John Colagioia <john@colagioia.net>
2018-12-26 07:05:33 -0500

    Hash country code to name

--- people/generate.js
+++ people/generate.js
@@ -3,6 +3,31 @@ const fs = require('fs');
 const population = loadGrid('population_density_deg.csv');
 const countries = loadGrid('country_deg.csv');
 const ciafact = JSON.parse(fs.readFileSync('factbook.json'));
+const countrycodes = {};
+
+// Build a hash table of country code to country name
+const lines = fs
+  .readFileSync('country-3166-1.csv')
+  .toString()
+  .split('\n');
+
+for (let i = 0; i < lines.length; i++) {
+  const parts = lines[i].split(',');
+  
+  if (parts.length < 2) {
+    break;
+  }
+
+  let name = parts[1];
+
+  if (name[0] === '"') {
+    for (let j = 2; j < parts.length; j++) {
+      name += `,${parts[j]}`;
+    }
+  }
+
+  countrycodes[parts[0]] = name.replace(/"/g, '');
+}
 
 function loadGrid(filename) {
   const result = [];
